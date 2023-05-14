ceefe4bbc0496cc907fe2357c7205e9d00c7ac6d
Amrit Mandal <83923234+amritPVre@users.noreply.github.com>
2023-05-15 00:57:38 +0530

    Update webscrapper_v01.py

--- webscrapper_v01.py
+++ webscrapper_v01.py
@@ -5,32 +5,6 @@ import csv
 import datetime
 import os
 import google.auth
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-
-# Define the Streamlit app
-def app():
-    # Check if user is already authenticated
-    creds = None
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', ['https://www.googleapis.com/auth/userinfo.email'])
-
-    # If not authenticated, authenticate user
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file('client_secret.json', ['https://www.googleapis.com/auth/userinfo.email'])
-            creds = flow.run_local_server(port=0)
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
-
-
-
-
-
 
 
 # Define function to scrape news headlines from RSS feed
